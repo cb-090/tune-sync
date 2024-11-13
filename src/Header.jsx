1ba11248685fb6037651fbf9ed11e39211c84fb3
cb-090 <cbabin2005@gmail.com>
2024-11-13 13:08:28 -0800

    Initial layout and styling of website

---
+++ src/Header.jsx
@@ -0,0 +1,8 @@
+export default function Header() {
+    return <header>
+        <div></div>
+        <h1>Tune Sync</h1>
+        <button id="SignIn">Sign In</button>
+    </header>
+    
+}
---
+++ src/Users.jsx
@@ -0,0 +1,7 @@
+export default function Users() {
+    return <nav>
+        <li>Cara</li>
+        <li>Noa</li>
+        <li>Tara</li>
+    </nav>
+}
